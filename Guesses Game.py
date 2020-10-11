82728402f8694b46237139f08293beeededa277a
Parveen Biswas <68769725+parveenbiswas8100@users.noreply.github.com>
2020-10-11 14:13:23 +0530

    Update Guesses Game.py
    Again upload

--- Guesses Game.py
+++ Guesses Game.py
@@ -4,16 +4,17 @@ print("You have only five guesses")
 print("the number in between 1 to 99")
 
 ans_win = 97
-gues = 1
+gues = 0
+
 while(gues<=5):
     user_put = int(input("Enter your number:\n"))
-    print("You have", 5 - gues, "left")
+    print("You have", 5 - gues - 1, "left")
     gues = gues + 1
     if user_put<ans_win:
         print("your guess is to low...")
     elif user_put==ans_win:
         print("congrates, you won")
-        print("you won in", gues)
+        print("you won in", gues, "Gues")
         break
     else:
         print("Your guess is to high...")
